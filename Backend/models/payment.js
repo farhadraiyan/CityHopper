07dd499153c7a4cb28ffdee4ebc117eb0f3c73fe
igorMori <igor_canada@outlook.com>
2019-01-17 18:12:42 -0500

    schema
    sample schema

---
+++ Backend/models/payment.js
@@ -0,0 +1,10 @@
+
+const mongoose = require('mongoose');
+
+// ***** Build Your Model Schema here *****
+const earnings = mongoose.Schema({
+   
+
+})
+
+module.exports = mongoose.model('earnings', earningsSchema);
