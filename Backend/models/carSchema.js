8703ceaafb4aabf117cbd951f4e25523d394830b
Arifur Rahman <arifur.rahman@georgebrown.ca>
2019-01-17 18:39:31 -0500

    Car Scehema Added

---
+++ Backend/models/carSchema.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+// Car Schema 
+
+const carSchema = mongoose.Schema({
+    make: {
+        type: String,
+        require: true
+    },
+    model: {
+        type: String,
+        require: true
+    },
+    year: {
+        type: String,
+        require: true
+    },
+    color: {
+        type: String,
+        require: true
+    },
+    type: {
+        type: String,
+        require: true
+    },
+    licencePlateNum: {
+        type: String,
+        require: true
+    },
+    luggageCapacity: {
+        type: String,
+        require: true
+    }
+    
+})
+
+module.exports = mongoose.exports('cars', carSchema);
