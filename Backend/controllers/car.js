801d6a1c4a9305b49f534bf16b2503e69815e96c
Yash Thanki <yash@unoapp.com>
2019-01-29 19:09:45 -0500

    create car routes

--- Backend/controllers/car.js
+++ Backend/controllers/car.js
@@ -50,3 +50,56 @@ let findCarByUserId = async function (req, res) {
         })
     }
 }
+
+let createCar = async function(req, res) {
+    let errors = {}
+    let reqFields = ['userId', 'make', 'model', 'year', 'color', 'type', 
+                    'seatCapacity', 'licencePlateNum', 'luggageCapacity']
+    // add fields to error if errors getting user information
+    reqFields.forEach(function(field) {
+        if(!req.body[field] || req.body[field] === '') {
+            errors[field] = `${field.replace(/_/g, ' ')} is required`
+        }
+    })
+    // send error
+    if(Object.keys(errors).length) {
+        return res.status(400).send({
+            msg: 'error creating car',
+            errors: errors,
+        })
+    }
+    let data = _.pick(req.body, reqFields)
+    
+    let createdCar
+    // try catch to catch error creating a car
+    try {
+        let createdCar = await new car(data)
+    } catch (error) {
+        return res.status(400).send({
+            message: "error creating car",
+            error: error
+        })
+    }
+    let savedCar
+    // try catch to catch error for saving created car to mongodb
+    try {
+        savedCar = await createdCar.save()
+        if (!savedCar) {
+            return res.status(400).send({
+                msg: 'error saving car'
+            })
+        }
+
+        // if everything works
+        return res.status(200).send({
+            msg: 'Vehicle successfully created',
+            vehicle: savedCar
+        })
+
+    } catch (error) {
+        return res.status(400).send({
+            msg: 'error saving car',
+            errors: error,
+        })
+    }
+}
--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const passport = require('passport')
-const dbUrl = 'mongodb://cityhopper:cityhopper1@ds113845.mlab.com:13845/capstone_project_db';
+const dbUrl = 'mongodb://admin:cityhopper123@ds131814.mlab.com:31814/cityhopper';
 
 const app = express();
 
