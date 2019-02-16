2156172521fb0bd3ad040d800be6543e9b6f133d
igorMori <igor_canada@outlook.com>
2019-02-16 01:25:17 -0500

    home trip auto fill and redirection
    home and redirection

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
     component: RegisterComponent
   },
   {
-    path: 'posttrip',
+    path: 'postTrip',
     component: PosttripComponent
   },
   {
--- frontend/src/app/body/user/profile-page/profile-page.component.ts
+++ frontend/src/app/body/user/profile-page/profile-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbRatingConfig} from '@ng-bootstrap/ng-bootstrap';
+import {Headers, Http} from '@angular/http';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
+import {Router} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -12,13 +16,22 @@ import {NgbRatingConfig} from '@ng-bootstrap/ng-bootstrap';
 })
 export class ProfilePageComponent implements OnInit {
 
-  constructor(config: NgbRatingConfig) {
+  headers:any;
+  title:any="aushuia";
+
+  constructor(config: NgbRatingConfig,private router:Router,private _route: ActivatedRoute) {
     // customize default values of ratings used by this component tree
     config.max = 5;
     config.readonly = true;
+
+
   }
 
   ngOnInit() {
+    const head = new Headers();
+    this.headers =head.values()
+
+    this._route.queryParams.subscribe(params => {this.title = params['location'];})
 
   }
 
