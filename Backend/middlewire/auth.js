4387ee5fe6698588baecc144fc0a35e2e36ad7e5
Yash Thanki <yashbthanki2@gmail.com>
2019-02-16 00:44:48 -0500

    JWT

--- Backend/middlewire/auth.js
+++ Backend/middlewire/auth.js
@@ -1,15 +1,21 @@
 const jwt = require('jsonwebtoken');
-const fs = require('fs');
 const privateKey = require('./../config/config')
 module.exports = checkJWT = async function(req, res, next) {
   let passedToken = req.headers.authorization.split(' ')[1]
-  let decodedToken = await jwt.verify(passedToken, privateKey)
+  try {
+    let decodedToken = await jwt.verify(passedToken, privateKey)
 
-  if (!decodedToken || decodedToken == '') {
+    if (!decodedToken || decodedToken == '') {
+      return res.status(403).send({
+        message: 'Unauthorised Request',
+        error: "Invalid JWT Token"
+      })
+    }
+    next()
+  } catch (error) {
     return res.status(403).send({
-      message: 'Unauthorised Request',
-      error: "Invalid JWT Token"
+      message: 'Error Authenticating',
+      error: "Check JWT"
     })
   }
-  next()
 }
