8a449e8734f2016cd2c5b6f3fb09a52a60af745c
igorMori <igor_canada@outlook.com>
2019-02-18 02:58:32 -0500

    bugs fixed
    bugs fixed

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/data-service/authentication.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private authinticateService:AuthenticationService,private router:Router) { }
+  login:any;
+  user:any;
   ngOnInit() {
-  }
 
+    this.login =this.authinticateService.getUserDetails()
+    console.log(this.login)
+  }
+  logout(){
+
+    this.authinticateService.logout();
+  }
 }
