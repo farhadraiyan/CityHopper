fee02f85a0581734cc7811d95eef665ffdef1b2d
igorMori <igor_canada@outlook.com>
2019-02-19 20:11:15 -0500

    Update user-data.service.ts

--- frontend/src/app/data-service/user-data.service.ts
+++ frontend/src/app/data-service/user-data.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import {Headers, Http} from '@angular/http';
+import {throwError as observableThrowError, Observable} from 'rxjs';
+import 'rxjs/add/operator/map';
+import {User} from '../models/User';
+import { map, filter, catchError, mergeMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserDataService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getOne(userId): Observable<any> {
+    let header = new HttpHeaders();
+    header.append('Content-Type','application/json');
+    return this.http.get(`http://localhost:3000/user/find/${userId}`).pipe(map(get =>{
+       get => get.json();
+       return get;
+    }));
+
+  }
 }
