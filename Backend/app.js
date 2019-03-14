166eac985546c0e4b122baa8dc346c3037fa65d4
Yash Thanki <yash@unoapp.com>
2019-03-14 15:20:11 -0400

    Messages Backend route

--- Backend/app.js
+++ Backend/app.js
@@ -3,8 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const passport = require('passport')
-const mongoose=require('mongoose')
-const cors=require('cors')
+const mongoose = require('mongoose')
+const cors = require('cors')
 var app = express();
 //the cor has to be use before the route
 app.use(cors());
@@ -12,16 +12,17 @@ app.use(cors());
 // require passport config and user Model
 require('./models/user');
 require('./config/passport');
-const config=require("./config/dbconfig")
+const config = require("./config/dbconfig")
 //database setup
 config.connectMlabDb()
-    .then(()=>console.log("connected to database"))
-    .catch((err)=>console.error("cannot connect to database",err))
+    .then(() => console.log("connected to database"))
+    .catch((err) => console.error("cannot connect to database", err))
 
 var usersRoutes = require('./routes/users');
 var carRoutes = require('./routes/car');
-let placeRoutes=require('./routes/place');
-let tripRoutes=require("./routes/trip");
+let placeRoutes = require('./routes/place');
+let tripRoutes = require("./routes/trip");
+let messageRoutes = require('./routes/message')
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -35,6 +36,6 @@ app.use(passport.initialize());
 app.use('/user', usersRoutes);
 app.use('/car', carRoutes);
 app.use('/place', placeRoutes);
-app.use('/trip',tripRoutes)
+app.use('/trip', tripRoutes)
 
 module.exports = app;
