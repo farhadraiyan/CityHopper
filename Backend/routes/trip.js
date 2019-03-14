571bcc4a9bc251897933bd67fc0c6c2fbdfa8c95
Yash Thanki <yash@unoapp.com>
2019-03-14 18:19:54 -0400

    Creating Trip Request

--- Backend/routes/trip.js
+++ Backend/routes/trip.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const trip = require('../controllers/trip');
 
-router.post('/create',trip.createTrip)
+router.post('/create', trip.createTrip)
+router.post('/createTripRequest', trip.createTripRequest)
 
-module.exports=router;
+module.exports = router;
