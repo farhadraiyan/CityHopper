f51bc832e0846da2c9d1b5926a935d84bb22437c
Yash Thanki <yash@unoapp.com>
2019-03-14 18:18:06 -0400

    Creating Trip Request

--- Backend/controllers/trip.js
+++ Backend/controllers/trip.js
@@ -1,71 +1,125 @@
 const TRIP = require('../models/trip')
+const TripRequest = require('../models/trip')
 const _ = require('lodash');
-const erroHandler=require("../library/errorhandlers")
-const User=require("../models/user")
-// let findAllTrip = async function (req, res) {
-//     TRIP.find().then((success) => {
-//         res.send(success)
-//     }).catch((error)=>{
-//         res.status(500).send(error)
-//     })
-// }
+const erroHandler = require("../library/errorhandlers")
+const User = require("../models/user")
 
 let createTrip = async function (req, res) {
-    //from to has type issue need to fix
-    let reqField=["from", "to","cost","departureTime","luggage","seatsAvailable","arivalTime","driver",
-"passengers","rating","car"]
-    //erro handle with Joi
-    let joiResult=erroHandler.joiConfigTrip(req.body);
-    //if error handler return errors
-    if(joiResult.error)
-    {
-        res.status(400).send(erroHandler.tripErrors(joiResult.error.details))
-        return;
-    }
+  //from to has type issue need to fix
+  let reqField = ["from", "to", "cost", "departureTime", "luggage", "seatsAvailable", "arivalTime", "driver",
+    "passengers", "rating", "car"]
+  //erro handle with Joi
+  let joiResult = erroHandler.joiConfigTrip(req.body);
+  //if error handler return errors
+  if (joiResult.error) {
+    res.status(400).send(erroHandler.tripErrors(joiResult.error.details))
+    return;
+  }
 
-    let tripData=_.pick(req.body, reqField)
+  let tripData = _.pick(req.body, reqField)
 
-    let createTrip;
-    try{
-        createTrip= await new TRIP(tripData)
-        //this is how get access to the user
-        const user=await User.findById(createTrip.driver)
-        console.log(user)
-    }catch(error)
-    {
-        return res.status(400).send({
-            message: "cannot create trip",
-            error: error
-        })
+  let createTrip;
+  try {
+    createTrip = await new TRIP(tripData)
+    //this is how get access to the user
+    const user = await User.findById(createTrip.driver)
+    console.log(user)
+  } catch (error) {
+    return res.status(400).send({
+      message: "cannot create trip",
+      error: error
+    })
 
+  }
+  let savedTrip;
+  try {
+    savedTrip = await createTrip.save();
+    if (!savedTrip) {
+      return res.status(404).send({
+        msg: 'cannot save trip'
+      })
     }
-    let savedTrip;
-    try{
-        savedTrip=await createTrip.save();
-        if (!savedTrip) {
-            return res.status(404).send({
-                msg: 'cannot save trip'
-            })
-        }
-                // if all good
-                return res.status(200).send({
-                    msg: 'trip created successfully',
-                    place: savedTrip
-                })
-        
-    }
-    catch(error)
-    {
-        return res.status(400).send({
-            msg: 'error saving trip',
-            errors: error,
-        })
-    }
+    // if all good
+    return res.status(200).send({
+      msg: 'trip created successfully',
+      place: savedTrip
+    })
 
-    
+  }
+  catch (error) {
+    return res.status(400).send({
+      msg: 'error saving trip',
+      errors: error,
+    })
+  }
 }
 
-module.exports={
-    // findAllTrip,
-    createTrip
+let createTripRequest = async function (req, res) {
+  // get the trip ID
+  let errors = {}
+  let reqFields = ['tripId', 'riderId', 'seatsRequested', 'additionalDetails']
+  // add fields to error if errors getting user information
+  reqFields.forEach(function (field) {
+    if (!req.body[field] || req.body[field] === '') {
+      errors[field] = `${field.replace(/_/g, ' ')} is required`
+    }
+  })
+  if (Object.keys(errors).length) {
+    return res.status(400).send({
+      msg: 'error creating trip request',
+      errors: errors,
+    })
+  }
+  let data = _.pick(req.body, reqFields)
+
+  let trip
+  try {
+    trip = await TRIP.findById(data.tripId)
+  } catch (error) {
+    return res.status(400).send({
+      message: 'Error fetching trip, check trip ID',
+      error: error.message
+    })
+  }
+  let newTripReq
+  try {
+    newTripReq = await new TripRequest(data)
+  } catch (error) {
+    return res.status(400).send({
+      message: 'Error creating trip request',
+      message: error.message
+    })
+  }
+  let savedTripRequest
+  try {
+    savedTripRequest = await newTripReq.save()
+  } catch (error) {
+    return res.status(400).send({
+      message: 'Error saving trip request',
+      error: error.message
+    })
+  }
+
+  let savedTripWithTripRequest
+  try {
+    savedTripWithTripRequest = await trip.addTripRequest(savedTripRequest._id)
+  } catch (error) {
+    return res.status(400).send({
+      message: 'Error saving trip request to Trip',
+      error: error.message
+    })
+  }
+  res.status(200).send({
+    message: 'success',
+    data: {
+      trip: savedTripWithTripRequest,
+      tripRequest: savedTripRequest
+    }
+  })
+}
+
+module.exports = {
+  // findAllTrip,
+  createTrip,
+  createTripRequest
 }
--- Backend/models/trip.js
+++ Backend/models/trip.js
@@ -1,54 +1,89 @@
 const mongoose = require('mongoose')
 //const Place = require('./place')
-
+const TripRequestSchema = mongoose.Schema({
+  riderId: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'user',
+    required: true
+  },
+  seatsRequested: {
+    type: Number,
+    required: true
+  },
+  Confirmed: {
+    type: Boolean
+  },
+  time_created: {
+    type: Date,
+    default: Date.now
+  },
+  additionalDetails: {
+    type: String
+  }
+})
 const Trip = mongoose.Schema({
-    from: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'place',
-        required: true
-    },
-    to: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'place',
-        required: true
-    },
-    cost: {
-        type: Number,
-        required: true
-    },
-    departureTime:{ 
-        type: Date, 
-        default: Date.now 
-    },
-    arivalTime:{ 
-        type: Date, 
-        default: Date.now 
-    },
-    seatsAvailable:{
-        type:Number,
-        required:true
-        
-    },
-    luggage:{
-        type:String,
-        required:true
-    },
-    driver: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'user'
-    },
-    passengers: [{
-        type: mongoose.Schema.ObjectId,
-        ref: 'user'
-    }],
-    rating: {
-        type: Number,
-        require: true
-    },
-    car: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'car'
-    }
+  from: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'place',
+    required: true
+  },
+  to: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'place',
+    required: true
+  },
+  cost: {
+    type: Number,
+    required: true
+  },
+  departureTime: {
+    type: Date,
+    default: Date.now
+  },
+  arivalTime: {
+    type: Date,
+    default: Date.now
+  },
+  seatsAvailable: {
+    type: Number,
+    required: true
+  },
+  luggage: {
+    type: String,
+    required: true
+  },
+  driver: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'user'
+  },
+  passengers: [{
+    type: mongoose.Schema.ObjectId,
+    ref: 'user'
+  }],
+  rating: {
+    type: Number,
+    require: true
+  },
+  car: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'car'
+  },
+  tripRequests: [{
+    type: mongoose.Schema.ObjectId,
+    ref: 'Trip'
+  }]
 })
 
+Trip.methods.addTripRequest = async function (tripReqID) {
+  this.TripRequests.push(tripReqID)
+  let updatedTrip
+  try {
+    updatedTrip = await this.save()
+  } catch (error) {
+    return error
+  }
+  return updatedTrip
+}
+
+module.exports = mongoose.model('TripRequest', TripRequestSchema)
 module.exports = mongoose.model('Trip', Trip);
