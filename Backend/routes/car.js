7286c4269fb61d65cc0e84c893b804da12ad4da2
Yash Thanki <yash@unoapp.com>
2019-03-19 17:47:45 -0400

    Car fix

--- Backend/routes/car.js
+++ Backend/routes/car.js
@@ -4,10 +4,10 @@ const car = require('../controllers/car');
 
 /* GET car listing. */
 router.get('/find', car.findCarByCarId);
-router.get('/find/All',car.findAll);
+router.get('/find/All', car.findAll);
 router.get('/find/user', car.findCarByUserId);
-router.post('/register/:_id', car.createCar);
+router.post('/register/', car.createCar);
 router.patch('/update', car.updateCar)
-router.delete('/delete',car.deleteCar);
+router.delete('/delete', car.deleteCar);
 
 module.exports = router;
