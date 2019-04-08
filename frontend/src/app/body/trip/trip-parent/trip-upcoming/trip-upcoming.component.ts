be2c92121108cd5711347ccf523aaf93b634ab6b
Yash Thanki <yash@unoapp.com>
2019-04-07 22:57:13 -0400

    Trip Requests

--- frontend/src/app/body/trip/trip-parent/trip-upcoming/trip-upcoming.component.ts
+++ frontend/src/app/body/trip/trip-parent/trip-upcoming/trip-upcoming.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../../../data-service/authentication.service';
-import {Headers, Http} from '@angular/http';
+import { Headers, Http } from '@angular/http';
 @Component({
   selector: 'app-trip-upcoming',
   templateUrl: './trip-upcoming.component.html',
@@ -10,11 +10,11 @@ export class TripUpcomingComponent implements OnInit {
 
   constructor(private authService: AuthenticationService, private http: Http) { }
   trips: any = []
-  userID =  this.authService.getUserDetails()['_id']
+  userID = this.authService.getUserDetails()['_id']
   ngOnInit() {
     this.getAllTripRequests()
   }
-  getAllTripRequests () {
+  getAllTripRequests() {
     this.http.get(`http://localhost:3000/trip/find/upcomingTrips/${this.userID}`).toPromise().then((res) => {
       this.trips = res['data']
       console.log(JSON.parse(res['_body'])['data'])
@@ -22,6 +22,11 @@ export class TripUpcomingComponent implements OnInit {
     }).catch((err) => {
       console.log(err)
     })
+  }
+
+  getFormatedDate(date) {
+    let D_a_t_e = date.split('T')
+    return `${D_a_t_e[0]} at ${D_a_t_e[1].substr(0, 5)}`
   }
 
 }
